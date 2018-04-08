38ec77a5554a55070e183fc15dc6c6ae6033706b
sarupahari <sarupahari@gmail.com>
2018-04-08 17:32:59 -0500

    Added User and UserSetting Table

--- app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/database/ChangeExChDbSchema.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/database/ChangeExChDbSchema.java
@@ -14,6 +14,30 @@ public class ChangeExChDbSchema {
                 public static final String CURRENCYCODE = "currencyCode";
                 public static final String PRICE = "price";
                 public static final String THRESHOLD = "threshold";
+                public static final String USERNAME = "username";
+            }
+        }
+
+        public static final class UserTable{
+            public static final String NAME = "user";
+
+            public static final class Cols {
+                public static final String UUID = "uuid";
+                public static final String USERNAME = "username";
+                public static final String PASSWORD = "password";
+                public static final String CURRENCYPREFERENCE = "currencyPreference";
+            }
+        }
+
+        public static final class UserSettingsTable{
+            public static final String NAME = "userSetting";
+
+            public static final class Cols {
+                public static final String UUID = "uuid";
+                public static final String USERNAME = "username";
+                public static final String TIMEFORMAT = "timeFormat";
+                public static final String PRIORITYCURRENCYPREFERENCE = "priorityCurrencyPreference";
+                public static final String COLORMODE = "colorMode";
             }
         }
 }
--- app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/database/PriceAlarmBaseHelper.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/database/PriceAlarmBaseHelper.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import edu.wtamu.cis.cidm4385saru.changeexchain.database.ChangeExChDbSchema.PriceAlarmTable;
+import edu.wtamu.cis.cidm4385saru.changeexchain.database.ChangeExChDbSchema.*;
 /**
  * Created by sarup on 4/1/2018.
  */
@@ -12,7 +12,7 @@ import edu.wtamu.cis.cidm4385saru.changeexchain.database.ChangeExChDbSchema.Pric
 
 public class PriceAlarmBaseHelper extends SQLiteOpenHelper {
     private static final int VERSION = 1;
-    private static final String DATABASE_NAME = "priceAlarmBase.db";
+    private static final String DATABASE_NAME = "changeExChain.db";
 
     public PriceAlarmBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, VERSION);
@@ -20,13 +20,34 @@ public class PriceAlarmBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("create table " + UserTable.NAME + "(" +
+                " _id integer primary key autoincrement, " +
+                UserTable.Cols.USERNAME + ", " +
+                UserTable.Cols.PASSWORD+ ", " +
+                UserTable.Cols.CURRENCYPREFERENCE +
+                ")"
+        );
+
+        db.execSQL("create table " + UserSettingsTable.NAME + "(" +
+                " _id integer primary key autoincrement, " +
+                UserSettingsTable.Cols.TIMEFORMAT + ", " +
+                UserSettingsTable.Cols.PRIORITYCURRENCYPREFERENCE + ", " +
+                UserSettingsTable.Cols.COLORMODE + ", " +
+                "FOREIGN KEY ("+ UserSettingsTable.Cols.USERNAME +") REFERENCES "+ UserTable.NAME +"(username)" +
+                ")"
+        );
+
         db.execSQL("create table " + PriceAlarmTable.NAME + "(" +
                 " _id integer primary key autoincrement, " +
                 PriceAlarmTable.Cols.CURRENCYCODE + ", " +
                 PriceAlarmTable.Cols.PRICE + ", " +
-                PriceAlarmTable.Cols.THRESHOLD +
+                PriceAlarmTable.Cols.THRESHOLD + ", " +
+                "FOREIGN KEY ("+ PriceAlarmTable.Cols.USERNAME +") REFERENCES "+ UserTable.NAME +"(username)" +
                 ")"
         );
+
+
     }
 
     @Override
