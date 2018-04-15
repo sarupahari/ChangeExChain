de1e0df7eca043164c1856d4bd778adfd284e0e4
sarupahari <sarupahari@gmail.com>
2018-04-15 12:53:06 -0500

    Added pricealarm fragment layout

--- app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/MainActivity.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.wtamu.cis.cidm4385saru.changeexchain;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmetManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmenById(R.id.fragmnet_container);
+
+        if (fragment == null)
+        {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
     }
 }
---
+++ app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/PriceAlarmFragment.java
@@ -0,0 +1,8 @@
+package edu.wtamu.cis.cidm4385saru.changeexchain;
+
+/**
+ * Created by sarup on 4/15/2018.
+ */
+
+public class PriceAlarmFragment {
+}
