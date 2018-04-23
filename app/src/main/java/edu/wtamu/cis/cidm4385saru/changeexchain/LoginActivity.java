53fa96e53d90b459d33b9494a00e91a5f8aa74b3
sarupahari <sarupahari@gmail.com>
2018-04-22 20:41:18 -0500

    Added link to the textview  for Powered by CoinDesk

--- app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/LoginActivity.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/LoginActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -31,6 +33,8 @@ public class LoginActivity  extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView text = (TextView) findViewById(R.id.exCreditCoinDesk);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
 
         mAuth = FirebaseAuth.getInstance();
 
--- app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/RegisterActivity.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/RegisterActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -33,6 +35,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        TextView text = (TextView) findViewById(R.id.exCreditCoinDesk);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
 
         mAuth = FirebaseAuth.getInstance();
 
