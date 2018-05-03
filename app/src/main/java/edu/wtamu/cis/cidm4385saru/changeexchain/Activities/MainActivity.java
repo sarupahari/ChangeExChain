a0050af0f6dfa98b22126f67f9c9c5dea4d4dae8
sarupahari <sarupahari@gmail.com>
2018-05-03 18:45:09 -0500

    Added the settings action

--- app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/Activities/MainActivity.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385saru/changeexchain/Activities/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toolbar;
 
 import edu.wtamu.cis.cidm4385saru.changeexchain.R;
 
@@ -26,5 +29,22 @@ public class MainActivity extends AppCompatActivity{
                 //TODO Add to where it takes you to the add an alarm view
             }
         });
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_action, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.menu.toolbar_action:
+                Intent settings = new Intent(MainActivity.this,UserSettingsActivity.class);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
